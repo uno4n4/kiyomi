8a2f2a93aff16c424693b65d5a403e437c680be0
laura-l77 <lauralebreton77@gmail.com>
2025-12-04 11:15:46 +0100

    import et export routes produits

--- kiyomi/src/app/app.routes.ts
+++ kiyomi/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Accueil} from './components/accueil/accueil';
 import { FormulaireCo } from './components/formulaire-co/formulaire-co';
+import { Produit } from './components/produit/produit';
 
 export const routes: Routes = [
     {path:'', component: Accueil},
     {path:'app-accueil', component: Accueil},
-    {path:'app-formulaire-co', component: FormulaireCo}
+    {path:'app-formulaire-co', component: FormulaireCo},
+    {path:'app-produit', component: Produit}
 ];
 //le premier path est la route par defaut: ''. Affichage de base
 //le deuxieme path est la route wildcard: '**'. Utile si la route n'existe pas
