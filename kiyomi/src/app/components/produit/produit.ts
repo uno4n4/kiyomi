c3a08df02f70e927384d6ce3e73ad97e8512182b
laura-l77 <lauralebreton77@gmail.com>
2025-12-04 11:09:17 +0100

    création page produit

---
+++ kiyomi/src/app/components/produit/produit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  imports: [],
+  templateUrl: './produit.html',
+  styleUrl: './produit.css',
+})
+export class Produit {
+
+}
