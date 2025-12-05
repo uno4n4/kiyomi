aba5ebcdd2ac82aa5337e9d2e3b5bd2148a3d992
diabasmr <diaba.samoura@hotmail.com>
2025-12-06 00:35:54 +0100

    Fonctionnement du service d'affichage des boxes
    Laura pourra l'utiliser pour sa page produits individuels

--- kiyomi/src/app/components/accueil/accueil.ts
+++ kiyomi/src/app/components/accueil/accueil.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListeBoxes } from '../../services/liste-boxes'; //import du service de recuperation des boxes
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
-export class Accueil {
+export class Accueil implements OnInit {
+  apiData: any[] = []; //variable pour stocker les données de l'API
 
+  constructor(private listeBoxes: ListeBoxes) {} //injection du service
+
+  ngOnInit(): void {
+    this.getDataFromAPI(); //appel de la methode au chargement du composant
+  }
+
+  getDataFromAPI() {
+    return this.listeBoxes.getBoxes().subscribe((menus) => {
+      this.apiData = menus; //stockage des données reçues dans la variable
+    });
+    }
 }
