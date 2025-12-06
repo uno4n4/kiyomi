e0e864a1e96f9cba663edf56fda16f2a71a64f8b
uno4n4 <sortlcrossvolee@gmail.com>
2025-12-06 19:20:28 +0100

    modif

--- kiyomi/src/app/app.ts
+++ kiyomi/src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('kiyomi');
+  user: any = null;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object){}
+  ngOnInit(){
+    if(isPlatformBrowser(this.platformId)){
+    const savedUser = localStorage.getItem('user');
+    if(savedUser){
+      this.user = JSON.parse(savedUser);
+    }
+  }
+}
 }
--- kiyomi/src/app/components/formulaire-co/formulaire-co.ts
+++ kiyomi/src/app/components/formulaire-co/formulaire-co.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-co',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulaire-co.html',
   styleUrl: './formulaire-co.css',
 })
 export class FormulaireCo {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(private auth: Auth) {}
+
+  onSubmit(){
+    this.auth.login(this.email, this.password).subscribe(res => {
+      if(res.success){
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('user', JSON.stringify(res.user));
+
+        console.log("Connecté :", res.user.prenom, res.user.nom);
+      } else {
+        this.error = "Identifiants incorrects";
+      }
+    },
+    err => {
+      this.error = "Erreur de connexion";
+    }
+  )
+  }
 }
